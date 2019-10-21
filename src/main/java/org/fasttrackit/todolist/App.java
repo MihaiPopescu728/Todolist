2e4a093a1b9c1f8b5e52d64afdec2d1009aacfd2
Mihai728 <mihaipopescu728@yahoo.com>
2019-10-21 20:05:00 +0300

    Added UPDATE DELETE getToDoItems

--- src/main/java/org/fasttrackit/todolist/App.java
+++ src/main/java/org/fasttrackit/todolist/App.java
@@ -1,20 +1,26 @@
 package org.fasttrackit.todolist;
 
+import org.fasttrackit.todolist.Domain.ToDoItem;
 import org.fasttrackit.todolist.persistance.ToDoItemRepository;
 import org.fasttrackit.todolist.transfer.CreateToDoItemRequest;
 
 import java.io.IOException;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) throws SQLException, IOException, ClassNotFoundException {
         CreateToDoItemRequest request = new CreateToDoItemRequest();
-        request.setDescription ("Lear JDBC");
+        request.setDescription("Learn JDBC");
         request.setDeadline(LocalDate.now().plusWeeks(1));
 
         ToDoItemRepository toDoItemRepository = new ToDoItemRepository();
         toDoItemRepository.createTdoDoItem(request);
+        toDoItemRepository.updateToDoItem(1,true);
+        toDoItemRepository.deleteToDoItem(1);
+        List<ToDoItem> toDoItems = toDoItemRepository.getToDoItems();
+        System.out.println(toDoItems);
 
 
     }
